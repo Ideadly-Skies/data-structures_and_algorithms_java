a17ceb22515fb023392bcb292f7b093c3e2ebcbd
Ideadly-Skies <Obie.kal22@gmail.com>
2025-06-25 14:40:44 +1000

    feat: added input validation using while loop

---
+++ chapter1 - java fundamentals/InputValidation.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class InputValidation {
+    
+    public static void main(String[] args){
+        // Setup
+        Scanner in = new Scanner(System.in);
+        System.out.print("Coffee (y/n)? ");
+        boolean checkingInput = true;
+
+        // Read the initial response
+        String response = in.next();        
+
+        // Loop until the user inputs a valid choice
+        while (checkingInput){
+            // If response is "y" or "n", end the loop
+            if (response.equalsIgnoreCase("y") || response.equalsIgnoreCase("n")){
+                checkingInput = false; 
+            }
+
+            // otherwise, propmt and read again
+            else {
+                System.out.print("Enter y or n: ");
+                response = in.next();
+            }
+        }
+    }    
+}
