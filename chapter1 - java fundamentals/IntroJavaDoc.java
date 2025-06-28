0026aefbc71f51770ed19900a23a7793dba7d695
IdeadlySkies <Obie.kal22@gmail.com>
2025-06-28 23:07:26 +0700

    feat: added intro to javadoc

---
+++ chapter1 - java fundamentals/IntroJavaDoc.java
@@ -0,0 +1,23 @@
+public class IntroJavaDoc {
+    
+    /**
+     * Roll a group of dice and return their sum
+     * 
+     * @param n number of dice
+     * @param faces number of faces on each die
+     * @return the sum of n random rolls in [1, faces]
+     */ 
+    public static int rollDice(int n, int faces){
+        int sum = 0;
+        for (int i = 0; i < n; i++){
+            sum += (int) (Math.random() * faces) + 1;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args){
+        // roll a 6 faced die 5 times and compute sum  
+        int sum = rollDice(5, 6);
+        System.out.println(sum);
+    }    
+}
