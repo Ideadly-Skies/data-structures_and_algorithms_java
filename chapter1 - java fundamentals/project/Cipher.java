5807d2a9103f4923970a7b732251cc26f6f37b87
Ideadly-Skies <Obie.kal22@gmail.com>
2025-06-29 17:43:25 +1000

    feat: implement vigenere cipher with key encryption decryption

--- chapter1 - java fundamentals/project/Cipher.java
+++ chapter1 - java fundamentals/project/Cipher.java
@@ -32,26 +32,92 @@ public class Cipher {
      * @param ciphertext
      * @param shift
      * @return
-     */
+    */
     public static String decrypt(String ciphertext, int shift) {
         String decrypted = "";
+        int rotIndex, index;        
 
         for(int i = 0; i < ciphertext.length(); i++){
-            char ch = ciphertext.charAt(i);
-            int index = ALPHABET.indexOf(ch);
-            int rotIndex = (index - shift) % 26;
-            char decryptedChar = ALPHABET.charAt(rotIndex);
-
-            decrypted += decryptedChar;
+            index = ALPHABET.indexOf(ciphertext.charAt(i)); 
+            if (index < shift) {
+                rotIndex = ALPHABET.length() + (index - shift);
+            } else {
+                rotIndex = index - shift; 
+            }
+            
+            decrypted += Character.toString(ALPHABET.charAt(rotIndex));
         }
 
         return decrypted;
     }
-    
+
+    /**
+     * Vigenere cipher encryption 
+     * 
+     * @param plaintext the input string
+     * @param key the key string
+     * @return the encrypted ciphertext string
+    */
+    public static String vigenereEncrypt(String plaintext, String key) {
+        // start with an empty output
+        String encryption = "";
+
+        // iterate over the letters in the plaintext
+        for (int i = 0; i < plaintext.length(); i++){
+            // get the ith letter of the plaintext 
+            char ch = plaintext.charAt(i);
+
+            // determine matching letter of the key 
+            // 
+            // use the mod operation to wrap around the key
+            int keyIndex = i % key.length();
+            char keyCh = key.charAt(keyIndex);
+
+            // use the key letter's alphabet position for the shift
+            int shift = ALPHABET.indexOf(keyCh) + 1;
+            
+            // encrypt the letter and append it to the output
+            // 
+            // encrypt needs a string input
+            // Character.toString(converts a character to a string)
+            encryption += encrypt(Character.toString(ch), shift);
+        }
+
+        // return the encrypted text
+        return encryption;
+    }
+
+    public static String vigenereDecrypt(String encrypted, String key) {
+        // start with an empty decryption
+        String decryption = "";
+
+        // iterate over all of the letters in the decrypt
+        for (int i = 0; i < encrypted.length(); i++){
+            // get the ith letter of the encrypted 
+            char ch = encrypted.charAt(i);
+
+            // determine matching letter of the key
+            // 
+            // use the mod operation to wrap around the key
+            int keyIndex = i % key.length();
+            char keyCh = key.charAt(keyIndex);
+
+            // use the key letter's alphabet position for the shift
+            int shift = ALPHABET.indexOf(keyCh) + 1;
+
+            // decrypt the letter and append it to the output
+            // 
+            // decrypt needs a string input
+            decryption += decrypt(Character.toString(ch), shift);
+        }
+
+        return decryption;
+    }
+
     /**
      * main -- entry point for the application
      * @param args
-     */
+    */
     public static void main(String[] args){
         
         String message = "TRAVELEAST";        
@@ -65,6 +131,21 @@ public class Cipher {
 
         // Decrypt the cipher
         String decrypted = decrypt(cipher, rotation);
+        System.out.printf("%s: %s\n", "The decrypted message: ", decrypted);
+
+
+        // Vigenere decryption 
+        message = "TRAVELEAST";
+        String key = "CAT";
+        
+        System.out.printf("%s: %s\n", "The original message: ", message);
+
+        // encrypt the message
+        cipher = vigenereEncrypt(message, key);
+        System.out.printf("%s: %s\n", "The encrypted message: ", cipher);
+        
+        // decrypt the message
+        decrypted = vigenereDecrypt(cipher, key);
         System.out.printf("%s: %s\n", "The decrypted message: ", decrypted);
     }
 }
