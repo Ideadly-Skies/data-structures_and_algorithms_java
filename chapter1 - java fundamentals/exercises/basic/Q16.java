a44b6e12764402151639bb281e93260bedc3688e
Ideadly-Skies <Obie.kal22@gmail.com>
2025-06-30 23:25:26 +1000

    feat: added Q16.java

--- chapter1 - java fundamentals/exercises/basic/Q16.java
+++ chapter1 - java fundamentals/exercises/basic/Q16.java
@@ -1,8 +1,19 @@
 package exercises.basic;
 
 public class Q16 {
-    public static void main(String[] args){
-        
+    public static boolean onlyConsonants(String str){
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
 
+            if (ch == 'a' || ch == 'i' || ch == 'u' || ch == 'e' || ch == 'o' || ch == 'y'){
+                return false; 
+            } 
+        }
+
+        return true;
+    } 
+    
+    public static void main(String[] args){
+        System.out.println(onlyConsonants("trtz"));
     } 
 }
