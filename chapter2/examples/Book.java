ad9a1683a1869bcf38a3d21fa2c1ee101f0b4fec
Ideadly-Skies <Obie.kal22@gmail.com>
2025-07-01 19:09:03 +1000

    feat(chapter2-examples): added custom constructor and toString() method

--- chapter2/examples/Book.java
+++ chapter2/examples/Book.java
@@ -8,4 +8,28 @@ public class Book {
     public String author;  
     public String title;
     public double price;
+
+    /**
+     * Constructor
+     * 
+     * @param newAuthor author of this new Book 
+     * @param newTitle title of this new Book
+     * @param newPrice price of this new Book
+     */
+    public Book(String newAuthor, String newTitle, double newPrice){
+        this.author = newAuthor;
+        this.title = newTitle; 
+        this.price = newPrice;
+    }
+
+    /**
+     * Returns a String representation of this Book
+     * 
+     * @return String describing title, author, and price 
+     */
+    public String toString(){
+        // Construct a summary string for this book
+        String summary = String.format("%s by %s, %.2f", this.title, this.author, this.price);
+        return summary;
+    } 
 }
--- chapter2/examples/BookDriver.java
+++ chapter2/examples/BookDriver.java
@@ -12,25 +12,28 @@ public class BookDriver {
     // unless you add the public keyword specifier before the type
     public static void main(String[] args){
         // Create a new Book object
-        Book prideAndPrejudice = new Book();
+        Book prideAndPrejudice = new Book("Sydney Khosasih", "Price and Prejudice", 9.99);
         
         // Assign values to its instance variables using dot notation
-        prideAndPrejudice.author = "Sydney Khosasih";
-        prideAndPrejudice.title = "Pride and Prejudice"; 
-        prideAndPrejudice.price = 9.99;
+        // prideAndPrejudice.author = "Sydney Khosasih";
+        // prideAndPrejudice.title = "Pride and Prejudice"; 
+        // prideAndPrejudice.price = 9.99;
 
         // print out book author title and price
-        System.out.printf("The book %s is written by %s and cost $%.2f\n",prideAndPrejudice.title, prideAndPrejudice.author, prideAndPrejudice.price);
+        // System.out.printf("The book %s is written by %s and cost $%.2f\n",prideAndPrejudice.title, prideAndPrejudice.author, prideAndPrejudice.price);
 
         // create a new Book object
-        Book algorithmsAndDataStructure = new Book();
+        Book algorithmsAndDataStructure = new Book("Jay Wengrow", "A Common-Sense Guide to Data Structures and Algorithms in Python, Volume 1: Level Up Your Core Programming Skills", 91.54);
 
         // assign values to its instance variables using dot notation
-        algorithmsAndDataStructure.author = "Jay Wengrow";
-        algorithmsAndDataStructure.title = "A Common-Sense Guide to Data Structures and Algorithms in Python, Volume 1: Level Up Your Core Programming Skills";
-        algorithmsAndDataStructure.price = 91.54;
+        // algorithmsAndDataStructure.author = "Jay Wengrow";
+        // algorithmsAndDataStructure.title = "A Common-Sense Guide to Data Structures and Algorithms in Python, Volume 1: Level Up Your Core Programming Skills";
+        // algorithmsAndDataStructure.price = 91.54;
 
         // print out book author title and price
-        System.out.printf("The book %s is written by %s and cost $%.2f\n",algorithmsAndDataStructure.title, algorithmsAndDataStructure.author, algorithmsAndDataStructure.price);
+        // System.out.printf("The book %s is written by %s and cost $%.2f\n",algorithmsAndDataStructure.title, algorithmsAndDataStructure.author, algorithmsAndDataStructure.price);
+    
+        System.out.println(prideAndPrejudice);
+        System.out.println(algorithmsAndDataStructure); 
     }
 }
